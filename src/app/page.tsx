f620a58f5cedf22b82e6bb81428203c276bb06fc
Henrique <87613130+Henricreate-09@users.noreply.github.com>
2025-08-08 21:20:57 -0300

    [Add]: Add colors in root element and greeting image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,13 @@ import styles from "@/styles/app.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-        
+      
+      <div id={styles.image}>
+        <div id={styles.greet}>
+          <h1>ASTRO STUDIES</h1>
+        </div>
+      </div>
+
     </div>
   );
 }
